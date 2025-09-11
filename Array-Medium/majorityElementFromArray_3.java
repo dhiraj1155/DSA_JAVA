beac4556aa71245fd9b6b3b6160e105ec29ce544
dhiraj <dhirajwagh2408@gmail.com>
2025-09-11 19:08:13 +0530

    finding majority element from an array

--- Array-Medium/majorityElementFromArray_3.java
+++ Array-Medium/majorityElementFromArray_3.java
@@ -25,19 +25,49 @@ public class majorityElementFromArray_3 {
     // }
     
     // better using hashing
+    // static int findMajorityElement(int arr[]){
+    //     int n = arr.length;
+    //     HashMap <Integer, Integer> mpp = new HashMap<>();
+
+    //     for(int i = 0; i<n; i++){
+    //         mpp.put(arr[i], mpp.getOrDefault(arr[i],0)+1);
+    //     }
+
+    //     for(int key : mpp.keySet()){
+    //         if(mpp.get(key) > n/2){
+    //             return key;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    // optimal using moore voting algo
     static int findMajorityElement(int arr[]){
         int n = arr.length;
-        HashMap <Integer, Integer> mpp = new HashMap<>();
+
+        int ele = 0;
+        int count = 0;
 
         for(int i = 0; i<n; i++){
-            mpp.put(arr[i], mpp.getOrDefault(arr[i],0)+1);
-        }
+            if(count == 0){
+                count = 1;
+                ele = arr[i];
+            }
+            else if(arr[i] == ele) count++;
+            else count --;
 
-        for(int key : mpp.keySet()){
-            if(mpp.get(key) > n/2){
-                return key;
+        }
+        int count1 = 0;
+
+        for(int i = 0; i<n; i++){
+            if(arr[i] == ele){
+                count1++;
             }
         }
+        if(count1 > n/2){
+            return ele;
+        }
         return -1;
+
     }
 }
